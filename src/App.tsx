f87a711851bf74eb6ff385ffc190d715d2f01049
Yelaman Karassay <yelaman@hkflair.org>
2025-10-04 23:49:24 +0800

    fix: update title and clean up routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import LangSelect from './ui/LangSelect';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './ui/HomePage';
 
 export default function ProgramTimeline() {
@@ -12,10 +11,10 @@ export default function ProgramTimeline() {
 
   return (
     <>
-    <Routes>
-      <Route path="/wedding" element={<LangSelect />} />
-      <Route path="/wedding/home" element={<HomePage />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   );
 }
